b1f375eb90f9ca7d2a396083af2442d92e5e7753
Misha Glushak <flatron_777@mail.ru>
2021-07-10 20:39:34 +1000

    Практическое задание к уроку №5. Рекурсия 1. Написать программу по возведению числа в степень с помощью рекурсии. 2. Написать программу «Задача о рюкзаке» с помощью рекурсии.

---
+++ src/main/java/arrays_and_sorting/homework5/Thing.java
@@ -0,0 +1,19 @@
+package arrays_and_sorting.homework5;
+
+public class Thing {
+    private final int weight;
+    private final int price;
+
+    public Thing(int weight, int price) {
+        this.weight = weight;
+        this.price = price;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+}
